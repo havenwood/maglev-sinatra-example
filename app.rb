0b2af56b3f6f76725dad505d92e966d27f1f3b2a
Shannon Skipper <shannonskipper@gmail.com>
2012-03-12 17:10:44 -0700

    simplified to make example more clear

--- app.rb
+++ app.rb
@@ -12,42 +12,30 @@ get '/' do
   erb :index
 end
 
-get '/people' do
-  Mag.pull
-  erb :people
-end
+post '/' do
+  name = params[:name]
 
-get '/person/add' do
-  Mag.pull
-  erb :add
-end
-
-post '/person/add' do
-  name = params[:name].split.first
-
-  redirect '/person/add' if name.nil? #this seems very wrong... return 40X?
+  redirect '/' if name.empty? #TODO: return appropriate http error
 
   name.capitalize!
 
   Mag.pull
 
-  unless Mag.box[:people].include? name
+  if Mag.box[:people].include? name
+    #TODO: return appropriate http error
+  else
     Mag.box[:people] << name
+    Mag.box[:people].sort!
     Mag.commit
   end
 
-  redirect '/person/add'
+  redirect '/'
 end
 
-get '/person/remove' do
-  Mag.pull
-  erb :remove
-end
+post '/remove' do
+  name = params[:name]
 
-post '/person/remove' do
-  name = params[:name].split.first
-
-  redirect 'person/remove' if name.nil?
+  redirect '/' if name.empty? #TODO: return appropriate http error
 
   name.capitalize!
 
@@ -56,16 +44,18 @@ post '/person/remove' do
   if Mag.box[:people].include? name
     Mag.box[:people].delete(name)
     Mag.commit
+  else
+    #TODO: return appropriate http error
   end
 
-  redirect '/person/remove'
+  redirect '/'
 end
 
 __END__
 
 @@layout
-<!doctype html>
-<html lang='en'>
+<!DOCTYPE html>
+<html lang='en' xmlns='http://www.w3.org/1999/xhtml' xml:lang='en'>
 <head>
   <title>API</title>
   <meta charset='utf-8' />
@@ -78,34 +68,23 @@ __END__
 </html>
 
 @@index
-<h1>Peeps</h1>
-<nav>
-  <li><a href='/person/add'>Add Person</a></li>
-  <li><a href='/person/remove'>Remove Person</a></li>
-</nav>
-
-@@add
-<h1>Add Person</h1>
-<p>
-  <% Mag.box[:people].each do |person| %>
-    <li><%= "#{person}" %></li>
-  <% end %>
-</p>
-<form action='/person/add' method='post'>
-  <input type='text' name ='name' placeholder='First name&hellip;'></input>  
-  <input type='submit' value='Add'>
-</form>
-<p><a href='/'>Back</a></p>
-
-@@remove
-<h1>Remove Person</h1>
-<p>
-  <% Mag.box[:people].each do |person| %>
-    <li><%= "#{person}" %></li>
-  <% end %>
-</p> 
-<form action='/person/remove' method='post'>
-  <input type='text' name ='name' placeholder='First name&hellip;'></input>  
-  <input type='submit' value='Remove'>
-</form>
-<p><a href='/'>Back</a></p>
+<section>
+  <h1>People</h1>
+  <ul>
+    <% Mag.box[:people].each do |person| %>
+      <li><%= "#{person}" %></li>
+    <% end %>
+  </ul>
+</section>
+<section>
+  <form action='/' method='post'>
+    <input type='text' name ='name' placeholder='First name' />  
+    <button type='submit'>Add</button>
+  </form>
+</section>
+<section>
+  <form action='/remove' method='post'>
+    <input type='text' name ='name' placeholder='First name' />  
+    <button type='submit'>Remove</button>
+  </form>
+</section>
